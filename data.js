e29867b67336e5e487f92e5aa1102fd52d28d698
RapeikaD <RapeikaD@gmail.com>
2018-10-15 13:41:18 +0300

    UpdatesV0.3
    +Points 1-3 from list

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports = {
+	
+		"site": "http://gmail.com/",
+		"EmailOne": "Retestd1@gmail.com",
+		"EmailTwo": "Retestd2@gmail.com",
+		"PassOne": "strongerpassword!",
+		"PassTwo": "strongerpassword!",
+		"timer": "5000"
+	
+}
--- spec.js
+++ spec.js
@@ -1,31 +1,44 @@
+//var info = require('./data.json')
+//var info = json.parse(data);
+//let package = require("/package.json")
+let data = require("./data.js")
+
 
 describe('This is first ptr test which', function() {
 	it("should send email and verify Sent email", function() {
 
 		browser.waitForAngularEnabled(false);
 
-		browser.get('http://gmail.com/');
-		element(by.name('identifier')).sendKeys('Retestd1@gmail.com');
+		//browser.get('http://gmail.com/');
+		browser.get(data.site)
+		//element(by.name('identifier')).sendKeys('Retestd1@gmail.com');
+		//element(by.name('identifier')).sendKeys(info.EmailOne);
+		element(by.name('identifier')).sendKeys(data.EmailOne);
 		element(by.id('identifierNext')).click();
 		browser.pause();
-		element(by.name('password')).sendKeys('strongerpassword!');
+		//element(by.name('password')).sendKeys('strongerpassword!');
+		element(by.name('password')).sendKeys(data.PassOne);
 		element(by.id('passwordNext')).click();
 		browser.wait(element(by.css(".T-I")).isPresent())
-  		browser.sleep(5000)
+  		//browser.sleep(5000)
+  		browser.sleep(data.timer)
   		browser.get("https://mail.google.com/mail/#inbox?compose=new")
-		browser.sleep(5000)
+		//browser.sleep(5000)
+		browser.sleep(data.timer)
 
 		var number = Math.round((Math.random()*100))
 		var RandomTitle = "Hello dear client # " + number
 
-		element(by.name('to')).sendKeys('Retestd2@gmail.com')
+		//element(by.name('to')).sendKeys('Retestd2@gmail.com')
+		element(by.name('to')).sendKeys(data.EmailTwo)
 		element(by.name('subjectbox')).sendKeys(RandomTitle)
 		element(by.className('Am Al editable LW-avf')).sendKeys('You are the best!')
 		
 		browser.actions().keyDown(protractor.Key.CONTROL).sendKeys('\uE007').perform();
 		browser.get("https://mail.google.com/mail/#sent")
-		browser.sleep(5000)
-		
+		//browser.sleep(5000)
+		browser.sleep(data.timer)
+
 		//this check if header equal to generated one, if so - verify if body is equal to template
 		let headerList = element.all(by.css('.bog'))
 		let list = element.all(by.css('.y2'))
@@ -33,7 +46,8 @@ describe('This is first ptr test which', function() {
 		for(var i = 0; i < headerList.length; i ++) {
 			if (headerList[i] == RandomTitle) {
 				expect(headerList.get(i).getText()).toBe(RandomTitle)
-				expect(list.get(i).getText()).toBe(' - You are the best!')
+				//expect(list.get(i).getText()).toBe(' - You are the best!')
+				expect(list.get(i).getText()).toBe(data.BodyText)
 				break;
 			}
 		}
@@ -47,11 +61,14 @@ describe('This is first ptr test which', function() {
 		browser.restart()
 		browser.waitForAngularEnabled(false);
 		browser.get('http://gmail.com/')
+		
 
-		element(by.name('identifier')).sendKeys('Retestd2@gmail.com');
+		//element(by.name('identifier')).sendKeys('Retestd2@gmail.com');
+		element(by.name('identifier')).sendKeys(data.EmailTwo);
 		element(by.id('identifierNext')).click();
 		browser.pause();
-		element(by.name('password')).sendKeys('strongerpassword!');
+		//element(by.name('password')).sendKeys('strongerpassword!');
+		element(by.name('password')).sendKeys(data.PassTwo);
 		element(by.id('passwordNext')).click();
 		browser.wait(element(by.css(".T-I")).isPresent())
 
@@ -61,7 +78,8 @@ describe('This is first ptr test which', function() {
 
 		for(var e = 0; e < unreadedLetters.length; e++)
 		if (unreadedLetters[e] == "test d" && inboxHeaders[e] == RandomTitle) {
-			expect(mailBody.get(i).getText()).toBe(' - You are the best!')
+			//expect(mailBody.get(i).getText()).toBe(' - You are the best!')
+			expect(mailBody.get(i).getText()).toBe(data.BodyText)
 			break;
 		}
 	})
